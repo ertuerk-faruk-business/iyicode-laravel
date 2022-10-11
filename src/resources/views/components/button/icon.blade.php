854d68a10127bad152660652d72e76108201e95f
Faruk Ertürk <ertuerk.faruk.business@gmail.com>
2022-10-11 16:40:06 +0200

    Add root styles for buttons

--- src/resources/views/components/button/icon.blade.php
+++ src/resources/views/components/button/icon.blade.php
@@ -1,14 +1,15 @@
-@props(['href', 'action', 'icon', 'target', 'border', 'background', 'padding', 'type'])
+@props(['href', 'action', 'icon', 'target', 'border', 'background', 'padding', 'type', 'root'])
 
 @isset($href)
-    <a href="{{ $href }}" target="{{ $target ?? '_self' }}">
+    <a href="{{ $href }}" target="{{ $target ?? '_self' }}" class="{{ $root ?? '' }}">
         <div
             class="{{ $icon ?? 'text-black hover:underline' }} {{ $background }} {{ $border }} {{ $padding ?? 'p-1' }} cursor-pointer">
             {{ $slot }}
         </div>
     </a>
 @else
-    <button type="{{ $type ?? 'button' }}" wire:click="{{ $action ?? null }}" class="outline-none p-0 m-0">
+    <button type="{{ $type ?? 'button' }}" wire:click="{{ $action ?? null }}"
+        class="outline-none p-0 m-0 {{ $root ?? '' }}">
         <div
             class="{{ $icon ?? 'text-black hover:underline' }} {{ $background }} {{ $border }} {{ $padding ?? 'p-1' }} cursor-pointer">
             {{ $slot }}
--- src/resources/views/components/button/solid.blade.php
+++ src/resources/views/components/button/solid.blade.php
@@ -1,14 +1,14 @@
-@props(['href', 'action', 'icon', 'target', 'border', 'background', 'text', 'padding', 'type'])
+@props(['href', 'action', 'icon', 'target', 'border', 'background', 'text', 'padding', 'type', 'root'])
 
 @isset($href)
-    <a href="{{ $href }}" target="{{ $target ?? '_self' }}">
+    <a href="{{ $href }}" target="{{ $target ?? '_self' }}" class="{{ $root ?? '' }}">
         <div
             class="{{ $text ?? 'text-black hover:underline' }} {{ $background }} {{ $border }} {{ $padding ?? 'px-4 py-2' }} text-center whitespace-nowrap select-none cursor-pointer">
             {{ $slot }}
         </div>
     </a>
 @else
-    <button type="{{ $type ?? 'button' }}" wire:click="{{ $action ?? null }}" class="outline-none p-0">
+    <button type="{{ $type ?? 'button' }}" wire:click="{{ $action ?? null }}" class="outline-none p-0 m-0 {{ $root ?? '' }}">
         <div
             class="{{ $text ?? 'text-black hover:underline' }} {{ $background }} {{ $border }} {{ $padding ?? 'px-4 py-2' }} text-center whitespace-nowrap select-none cursor-pointer">
             {{ $slot }}
