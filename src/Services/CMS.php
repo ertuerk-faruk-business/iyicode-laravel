3b9c6f659b7066eb12aeeb03ed12cf30c875279f
Faruk Ertürk <ertuerk.faruk.business@gmail.com>
2022-10-11 17:23:31 +0200

    Add CMS Support

---
+++ src/Services/CMS.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace IyiCode\Services;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Request;
+
+class CMS
+{
+    private $applicationToken;
+
+    public function __construct(mixed $applicationToken)
+    {
+        $this->applicationToken = $applicationToken;
+    }
+
+    public static function get(mixed $applicationToken): CMS
+    {
+        return new CMS($applicationToken);
+    }
+
+    public function visitor(mixed $ip = null): array|null
+    {
+        $cache = $this->getCachedVisitor($ip);
+
+        if (!empty($cache)) {
+            return $cache;
+        }
+
+        $response = Http::withHeaders([
+            'X_IYICMS_APPLICATION_TOKEN' => $this->applicationToken,
+        ])->post('https://cms.iyicode.com/api/application/analytics/save', [
+            'visitor' => $ip ?? Request::ip(),
+        ]);
+
+        if (!$response->ok()) {
+            return null;
+        }
+
+        $data = $response->json();
+
+        if ($data['status'] ?? 'error' == 'error') {
+            return null;
+        }
+
+        $formattedIp = str_replace('.', '_', $ip);
+
+        Cache::put('cms_iyicode_analytics_visitor_' . $formattedIp, $data, now()->addHour());
+
+        return $data;
+    }
+
+    private function getCachedVisitor(mixed $ip): array|null
+    {
+        $formattedIp = str_replace('.', '_', $ip);
+
+        $cache = Cache::get('cms_iyicode_analytics_visitor_' . $formattedIp) ?? null;
+
+        if (empty($cache)) {
+            return null;
+        }
+
+        return $cache;
+    }
+}
