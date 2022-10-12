f683af4c49c00adf61c0968b01ee4b8a37f59e9e
Faruk Ertürk <ertuerk.faruk.business@gmail.com>
2022-10-12 04:17:47 +0200

    Test

--- src/app/Livewire/View/Components/InputBuilder.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace IyiCode\App\Livewire\View\Components;
-
-class InputBuilder extends ComponentBuilder
-{
-    public mixed $class = Input::class;
-}
--- src/app/Support/Action/Response/Status.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace IyiCode\App\Support\Action\Response;
-
-class Status
-{
-    public static int $done = 0;
-    public static int $error = 1;
-}
