d619e18d31bfccbb80e9f03d31dfa144d1ca5465
Faruk Ertürk <ertuerk.faruk.business@gmail.com>
2022-10-14 01:54:02 +0200

    Improve loading overlay

--- src/resources/views/components/loading/overlay.blade.php
+++ src/resources/views/components/loading/overlay.blade.php
@@ -1,6 +1,6 @@
 @props(['target'])
 <div wire:loading @isset($target)wire:target="{{ $target }}"@endisset
-    class="fixed flex-col flex z-50 inset-0 bg-white bg-opacity-50 items-center justify-center w-screen h-screen">
+    class="fixed flex-col flex z-[9999] inset-0 bg-black bg-opacity-50 items-center justify-center w-screen h-screen">
     <div class="self-center flex items-center m-auto justify-center h-screen">
         <div class="spinner">
         </div>
