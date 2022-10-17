54d88413c61d463a6dfcff70e1ed9a2d9c597463
Faruk Ertürk <ertuerk.faruk.business@gmail.com>
2022-10-17 02:13:19 +0200

    Improve bug

--- src/resources/views/components/page/base/desktop.blade.php
+++ src/resources/views/components/page/base/desktop.blade.php
@@ -1,5 +1,5 @@
 @props(['sideBar', 'bottom'])
-<div x-data="{ showSideBar: {{ IyiCode\App\Support\View\SideBar::isDisabled() ? false : (IyiCode\App\Support\View\SideBar::isAlwaysVisible() ? true : "localStorage.getItem('showSideBar') === 'true'") }} }" class="hidden xl:block absolute z-0 container mx-auto left-0 right-0">
+<div x-data="{ showSideBar: {{ IyiCode\App\Support\View\SideBar::isDisabled() ? "false" : (IyiCode\App\Support\View\SideBar::isAlwaysVisible() ? "true" : "localStorage.getItem('showSideBar') === 'true'") }} }" class="hidden xl:block absolute z-0 container mx-auto left-0 right-0">
     <div class="flex flex-col w-full relative">
         <div class="flex flex-row w-full items-start bg-slate-100">
             @if (!IyiCode\App\Support\View\SideBar::isDisabled() && !empty($sideBar))
